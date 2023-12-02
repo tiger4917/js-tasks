8322e22b2cb64c2e97268f2471da0c71b0977ee1
tiger4917 <tiger491775@gmail.com>
2023-12-02 10:15:45 +0300

    Добавил task-2

---
+++ task-2/script.js
@@ -0,0 +1,13 @@
+let x = 357 //('Ivan') (true);
+if(typeof x === 'number'){
+    console.log(`${x} - число`);
+}
+else if(typeof x === 'string'){
+    console.log(`${x} - строка`);
+}
+else if(typeof x === 'boolean'){
+    console.log(`${x} - логический тип`);
+}
+else{
+    console.log(`Тип x не определён`);
+}
