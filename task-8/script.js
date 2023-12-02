e95e4048796f02951ed673245ee59dcf929dac6c
tiger4917 <tiger491775@gmail.com>
2023-12-02 10:33:41 +0300

    Добавил task-8

---
+++ task-8/script.js
@@ -0,0 +1,12 @@
+const cars = new Map([
+    ['mersedes', 'black'],
+    ['bmw', 'blue'],
+    ['toyota', 'white'],
+    ['ferrari', 'red'],
+    ['lamborgini', 'yellow']
+  ]);
+  for (let name of cars.keys()) {
+    console.log(name);
+  }
+  console.log(cars.entries());
+  console.log(cars.size);
