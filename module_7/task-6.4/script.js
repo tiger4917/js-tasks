6742f4abffd1cdaebefcaf7ea54bf1a6092a5cf4
tiger4917 <tiger491775@gmail.com>
2023-12-04 15:03:39 +0300

    Добавил решение task-6.5

---
+++ module_7/task-6.4/script.js
@@ -0,0 +1,11 @@
+// const ticTacToeField = [
+//     ['x', 'o', 'x'],
+//     ['o', 'x', 'o'],
+//     ['x', 'o', 'x']
+// ];
+
+// // Используем цикл для вывода поля в консоль
+// for (let i = 0; i < ticTacToeField.length; i++) {
+//     // Используем метод join для объединения элементов массива в строку с пробелами
+//     console.log(ticTacToeField[i].join(' '));
+// }
---
+++ module_7/task-6.5/script.js
@@ -0,0 +1,11 @@
+const obj = {
+    some: 'some',
+    dom: 'text',
+    arr: [1, 2, 3, 4, 5],
+    tom: 'there'
+};
+const arrValues = [];
+arrValues.push(Object.values(obj));
+console.log(arrValues.toString());
+
+
