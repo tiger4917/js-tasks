693157c65a79b6db7c4fae6e79720a5ffacfb067
tiger4917 <tiger491775@gmail.com>
2023-12-04 12:03:14 +0300

    Новая версия 6.1

--- module_7/task-6.1/script.js
+++ module_7/task-6.1/script.js
@@ -3,6 +3,7 @@ let word = prompt('Введите слово');
 let wordReverse = word.split('').reverse().join('');
 
 word === wordReverse ? console.log(`слово ${word} - является палиндромом`) : console.log(`слово ${word} - не является палиндромом`);
+
     
 
 
