e30b8f220448b9f19b9b8c860057d54d90909151
tiger4917 <tiger491775@gmail.com>
2023-12-04 15:07:53 +0300

    удалил задачу 6.4

--- module_7/task-6.4/script.js
+++ module_7/task-6.4/script.js
@@ -1,11 +1,1 @@
-// const ticTacToeField = [
-//     ['x', 'o', 'x'],
-//     ['o', 'x', 'o'],
-//     ['x', 'o', 'x']
-// ];
 
-// // Используем цикл для вывода поля в консоль
-// for (let i = 0; i < ticTacToeField.length; i++) {
-//     // Используем метод join для объединения элементов массива в строку с пробелами
-//     console.log(ticTacToeField[i].join(' '));
-// }
